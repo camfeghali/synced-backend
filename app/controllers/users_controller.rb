b99b3bf29479f4cb7ce6f6608f5700dc1593dbaa
camfeghali <camfeg@gmail.com>
2019-04-27 10:07:13 -0400

    implemented auth signup login and persistance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  # skip_before_action :authorized, only: [:create, :get_user, :index]
+  skip_before_action :authorized, only: [:create, :get_user, :index]
   #
-  # def index
-  #   @users = User.all
-  #   render json: @users
-  # end
+  def index
+    @users = User.all
+    render json: @users
+  end
   #
   def create
   @user = User.create(user_params)

fc67b7325fd4b174cd52251c84875fd598e8dd77
camfeghali <camfeg@gmail.com>
2019-04-30 14:23:41 -0400

    completed delete feature

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,5 +1,5 @@
 class PlaylistsController < ApplicationController
-  skip_before_action :authorized, only: [:create, :get_user, :index, :update]
+  skip_before_action :authorized, only: [:create, :get_user, :index, :update, :destroy]
 
   def index
     #code
@@ -15,6 +15,13 @@ class PlaylistsController < ApplicationController
     user = User.find_by(username: params["username"])
     playlist = Playlist.create(user: user, name: params["playlistName"])
     # byebug
+    render json: playlist
+  end
+
+  def destroy
+    # byebug
+    playlist = Playlist.find(params["id"])
+    playlist.destroy
     render json: playlist
   end
 
